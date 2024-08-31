14249edae5b636d41379b6dc7a14ec0c8f62e998
Tiago Alves <tiagolvs22@gmail.com>
2024-08-31 14:52:07 +0100

    Fix env config location

--- src/main/java/com/synctok/synctokApi/config/EnvConfig.java
+++ src/main/java/com/synctok/synctokApi/config/EnvConfig.java
@@ -1,4 +1,4 @@
-package com.synctok.synctokApi.client;
+package com.synctok.synctokApi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
