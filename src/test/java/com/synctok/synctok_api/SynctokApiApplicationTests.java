e1f339265a0140e55c5a8398ad801a4aacca9c29
Tiago Alves <tiagolvs22@gmail.com>
2024-08-31 11:18:49 +0100

    Initial commit

---
+++ src/test/java/com/synctok/synctok_api/SynctokApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synctok.synctok_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynctokApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
